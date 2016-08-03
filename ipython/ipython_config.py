dfcc2034f004b7087235d8ba87d9cb381d732e64
chad <chapnickc@gmail.com>
2016-08-03 16:14:05 -0500

    -

--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -4,16 +4,16 @@
 
 c = get_config()
 
-c.TerminalIPythonApp.display_banner = False
+c.TerminalIPythonApp.display_banner = True
 c.InteractiveShell.autoindent = False
 c.InteractiveShell.colors = 'Linux'
 c.InteractiveShell.editor = 'vim'
 #c.InteractiveShell.pylab = 'qt'
 
 #c.PromptManager.in_template  = '[\#] In: '
-c.PromptManager.in_template = r'{color.LightPurple}\${color.Cyan}[\#{color.Cyan}]{color.Cyan}: '
-c.PromptManager.out_template = '[\#] Out: '
-c.PromptManager.justify = True
+# c.TerminalInteractiveShell.prompts_class.in_template = r'{color.LightPurple}\${color.Cyan}[\#{color.Cyan}]{color.Cyan}: '
+# c.TerminalInteractiveShell.prompts_class.out_template = '[\#] Out: '
+# c.TerminalInteractiveShell.prompts_class.justify = True
 
 # set default figure window size for matplotlib
 #matplotlib.rcParams['figure.figsize'] = (15.0, 7.0)
